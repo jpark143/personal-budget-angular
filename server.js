f67a26ee400392eb2b85ecfa459123c49f81df7c
Jae Park <jae@April.local>
2020-10-06 11:53:48 -0400

    add cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const { json } = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/pb/', express.static('public'));
+//app.use('/pb/', express.static('public'));
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+//app.get('/hello', (req, res) => {
+//    res.send('Hello World!');
+//});
 
 app.get('/pb/budget', (req, res) => {
     
@@ -20,5 +22,5 @@ app.get('/pb/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}/pb`)
+    console.log(`API served at http://localhost:${port}/pb`)
 });
